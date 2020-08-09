57e5d55e61d1b05a847b5de77e3902977b2970c1
MedvedievM <medvediev.m91@gmail.com>
2020-08-09 20:50:20 +0300

    check login and password

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://Maksym:odk9JYdygYvbQ0KC@cluster0.0hxjg.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://Maksym:odk9JYdygYvbQ0KC@cluster0.0hxjg.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  jwt: 'dev-jwt'
 }
